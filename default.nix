9e78fb507c30d553270ad3c03ad5352eecee54bc
Jonathan Knowles <mail@jonathanknowles.net>
2019-02-11 11:30:05 +0000

    Add dependency on `servant-reflex`.

--- default.nix
+++ default.nix
@@ -2,9 +2,23 @@
 , iosSdkVersion ? "10.2"
 }:
 with import ./.obelisk/impl { inherit system iosSdkVersion; };
-project ./. ({ ... }: {
+project ./. ({ pkgs, ... }: {
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
   ios.bundleName = "Obelisk Minimal Example";
+
+  packages = {
+    servant-reflex = pkgs.fetchFromGitHub {
+      owner  = "imalsogreg";
+      repo   = "servant-reflex";
+      rev    = "ec8723351c8245f29a88cc5e6250533d2d6f4761";
+      sha256 = "1r8z95hpl6f5pql1f6a3plczahym09fdnxkcg3a6ildw6chmips0";
+    };
+  };
+
+  overrides = self: super: with pkgs.haskell.lib; {
+    servant-reflex = doJailbreak super.servant-reflex;
+  };
+
 })
