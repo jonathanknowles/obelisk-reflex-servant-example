32a3128123392a2241b11f7b9021b1e8c231f5b2
Yc.S <onixie@gmail.com>
2020-03-18 16:58:06 +0900

    Add ghcide

--- default.nix
+++ default.nix
@@ -20,5 +20,38 @@ project ./. ({ pkgs, ... }: {
   overrides = self: super: with pkgs.haskell.lib; {
     servant-reflex = doJailbreak super.servant-reflex;
   };
-
+  shellToolOverrides = ghc: super: {
+    ghcide = pkgs.haskell.lib.dontCheck ((ghc.override {
+      overrides = ghc: super: {
+        lsp-test =
+          pkgs.haskell.lib.dontCheck (ghc.callHackage "lsp-test" "0.6.1.0" { });
+        haddock-library = pkgs.haskell.lib.dontCheck
+          (ghc.callHackage "haddock-library" "1.8.0" { });
+        haskell-lsp = pkgs.haskell.lib.dontCheck
+          (ghc.callHackage "haskell-lsp" "0.19.0.0" { });
+        haskell-lsp-types = pkgs.haskell.lib.dontCheck
+          (ghc.callHackage "haskell-lsp-types" "0.19.0.0" { });
+        regex-posix = pkgs.haskell.lib.dontCheck
+          (ghc.callHackage "regex-posix" "0.96.0.0" { });
+        test-framework = pkgs.haskell.lib.dontCheck
+          (ghc.callHackage "test-framework" "0.8.2.0" { });
+        regex-base = pkgs.haskell.lib.dontCheck
+          (ghc.callHackage "regex-base" "0.94.0.0" { });
+        regex-tdfa = pkgs.haskell.lib.dontCheck
+          (ghc.callHackage "regex-tdfa" "1.3.1.0" { });
+        shake =
+          pkgs.haskell.lib.dontCheck (ghc.callHackage "shake" "0.18.4" { });
+        hie-bios = pkgs.haskell.lib.dontCheck (ghc.callHackageDirect {
+          pkg = "hie-bios";
+          ver = "0.4.0";
+          sha256 = "19lpg9ymd9656cy17vna8wr1hvzfal94gpm2d3xpnw1d5qr37z7x";
+        } { });
+      };
+    }).callCabal2nix "ghcide" (pkgs.fetchFromGitHub {
+      owner = "digital-asset";
+      repo = "ghcide";
+      rev = "v0.1.0";
+      sha256 = "1kf71iix46hvyxviimrcv7kvsj67hcnnqlpdsmazmlmybf7wbqbb";
+    }) { });
+  };
 })
